2dc5d5e62afae77bc19cc63a9767e8c88fa4b09f
lyuyan <you@example.com>
2022-05-27 00:06:15 +0700

    Fix code style

--- m.cpp
+++ m.cpp
@@ -2,14 +2,14 @@
 
 int main()
 {
-        std::cout<<"8 r n b q k b n r";
-        std::cout<<"7 p p p p p p p p";
-        std::cout<<"6";
-        std::cout<<"5";
-        std::cout<<"4";
-        std::cout<<"3";
-        std::cout<<"2 P P P P P P P P";
-        std::cout<<"1 R N B Q K B N R";
-        std::cout<<"  a b c d e f g h";
-        return 0;
+    std::cout << "8 r n b q k b n r";
+    std::cout << "7 p p p p p p p p";
+    std::cout << "6";
+    std::cout << "5";
+    std::cout << "4";
+    std::cout << "3";
+    std::cout << "2 P P P P P P P P";
+    std::cout << "1 R N B Q K B N R";
+    std::cout << "  a b c d e f g h";
+    return 0;
 }
